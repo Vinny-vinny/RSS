bf80411637b1c8126b993862235af62e03182c90
vinnyvinn <kituyiv@gmail.com>
2020-05-04 11:32:46 +0300

    category route

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 Route::view('/','home');
 Route::get('feed','VacancyController@feed');
-Route::get('categories','CategoryController@index');
 Route::view('/{any}','home');
 Route::view('/{any}/{any1}','home');
 
